ea214bede10f5347b7421f42f54e7c182e3c9ad2
MohammadFauzanAzima <fauzanazhima489@gmail.com>
2023-11-07 13:25:40 +0700

    hapus button tambah page history_peminjaman

--- resources/views/history_peminjaman.blade.php
+++ resources/views/history_peminjaman.blade.php
@@ -4,11 +4,11 @@
 <h1 class="h3 mb-2 text-gray-800">History Peminjaman</h1>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
-                        <div class="card-header py-3">
+                        {{-- <div class="card-header py-3">
                         <a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" type="button" data-toggle="modal" data-target="#exampleModalCenter">
                                 <i class="fas fa-plus fa-sm text-white-50"></i> Tambah
                             </a>
-                        </div>
+                        </div> --}}
                         <div class="card-body">
                             <div class="table-responsive">
                             <table class="table table-bordered text-center" id="dataTables" width="100%" cellspacing="0">
